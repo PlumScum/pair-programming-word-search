7f1831f9e309d77281f85bc8f5b6f1e98bf7b77c
Jelani Sanders <jelanisands@gmail.com>
2023-01-17 19:08:03 -0500

    Completed project

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,35 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function(array) {
+  const puzzle = [];
+  for (let i = 0; i < array[0].length; i++) {
+    const col = [];
+    for (let j = 0; j < array.length; j++) {
+      col.push(array[j][i]);
     }
-}
+    puzzle.push(col);
+  }
+  return puzzle;
+};
+const wordSearch = (letters, word) => {
+  if (letters.length === 0) {
+    return false;
+  }
+  let find = false;
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  const verticalJoin = transpose(letters).map(item => item.join(''));
 
-module.exports = wordSearch
+  for (nestedArr of horizontalJoin) {
+    if (nestedArr.includes(word)) {
+      find = true;
+    }
+    for (item of verticalJoin) {
+      if (item.includes(word)) {
+        find = true;
+      }
+
+    }
+
+
+  }
+  return find;
+};
+module.exports = wordSearch;
